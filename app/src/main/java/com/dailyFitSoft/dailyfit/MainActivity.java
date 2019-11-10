250e0d3baa9304e47c3e108a52f562c63bde4b0a
enshin258 <kuba.win@wp.pl>
2019-11-10 21:22:22 +0100

    robocza werjsa dodawania aktywnosci do kalendarza

--- app/src/main/java/com/dailyFitSoft/dailyfit/MainActivity.java
+++ app/src/main/java/com/dailyFitSoft/dailyfit/MainActivity.java
@@ -3,13 +3,14 @@ package com.dailyFitSoft.dailyfit;
 import android.app.Dialog;
 import android.os.Bundle;
 
-<<<<<<< HEAD
-import com.dailyFitSoft.dailyfit.ui.home.HomeFragment;
-=======
 import com.dailyFitSoft.dailyfit.dataStore.DataBaseHelper;
->>>>>>> 7ec410b66845454340566ad90b6ae0dce4866e64
+import com.dailyFitSoft.dailyfit.R;
+import com.dailyFitSoft.dailyfit.dataStore.DataBaseHelper;
+import com.dailyFitSoft.dailyfit.dataStore.Exercise;
+import com.dailyFitSoft.dailyfit.ui.home.HomeFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -18,6 +19,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.dailyFitSoft.dailyfit.dataStore.DataBaseHelper;
+
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -25,14 +28,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     public static String selectedDate="";
-
+    DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
+    String tempDataExercise="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,12 +91,52 @@ public class MainActivity extends AppCompatActivity {
     }
     public void addEventToCalendar()
     {
-        CalendarView homeCalendar = HomeFragment.getHomeCalendar();
         final Dialog dialog = new Dialog(MainActivity.this); // Context, this, etc.
         dialog.setContentView(R.layout.popup_add_event_to_calendar);
         dialog.setTitle(R.string.popup_title_add_event_to_calendar);
+
+
+        final Spinner exerciseSpinner = dialog.findViewById(R.id.exercise_selector);
+        List<String> exercises = new ArrayList<String>();
+        for (Exercise e:dataBaseHelper.getExerciseList()) {
+            exercises.add(e.getName());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, exercises);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        exerciseSpinner.setAdapter(adapter);
+        exerciseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                tempDataExercise = parentView.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+        final Button addExerciseButton = dialog.findViewById(R.id.add_exercise_button);
+        addExerciseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addExerciseToDay();
+                dialog.dismiss();
+
+            }
+        });
         TextView dateOfExerciseTextView = dialog.findViewById(R.id.date_of_exercise);
         dateOfExerciseTextView.setText("Exercise on: " + selectedDate);
         dialog.show();
+    }
+    public void addExerciseToDay()
+    {
+        ListView exerciseListView = findViewById(R.id.list_view_of_exercises);
+        List<String> exercisesData = new ArrayList<>();
+        exercisesData.add(tempDataExercise);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, exercisesData);
+        exerciseListView.setAdapter(adapter);
+
     }
 }
